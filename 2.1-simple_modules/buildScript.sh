7e6ecc963ca158ee2abfbf7884036102c8cc40d8
Kevin <>
2018-05-21 19:22:31 -0400

    Backup commit

--- 2.1-simple_modules/buildScript.sh
+++ 2.1-simple_modules/buildScript.sh
@@ -5,7 +5,8 @@ rm -rf ./build
 javac -d build/third.party.lib `find third.party.lib -type f -name '*.java'`
 javac -d build/kcs.my.lib -p build `find kcs.my.lib -type f -name '*.java'`
 javac -d build/kcs.app -p build `find kcs.app -type f -name '*.java'`
-echo Running MyApp
-java -p build -m kcs.app/my.app.notice.that.pckg.name.is.different.MyApp
+#echo Running MyApp
+#java -p build -m kcs.app/my.app.notice.that.pckg.name.is.different.MyApp
+#java -cp build/kcs.app:build/kcs.my.lib:build/third.party.lib my.app.notice.that.pckg.name.is.different.MyApp
 
 
