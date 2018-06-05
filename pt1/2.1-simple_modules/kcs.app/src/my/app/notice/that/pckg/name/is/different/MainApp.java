254c9187d9409ec144f649253c76fc1a8655cc6b
Kevin <>
2018-06-05 19:14:44 -0400

    Backup commit

--- pt1/2.1-simple_modules/kcs.app/src/my/app/notice/that/pckg/name/is/different/MainApp.java
+++ pt1/2.1-simple_modules/kcs.app/src/my/app/notice/that/pckg/name/is/different/MainApp.java
@@ -1,12 +1,13 @@
 package my.app.notice.that.pckg.name.is.different;
 
+import com.bleh.word.generator.impl.FrenchAnimalsWordGenerator;
 import kcs.lib.WordListBuilder;
 
 public class MainApp {
 
     public static void main(String[] args) {
         int itemQty = 10;
-        WordListBuilder wordListBuilder = new WordListBuilder();
+        WordListBuilder wordListBuilder = new WordListBuilder(new FrenchAnimalsWordGenerator());
         System.out.println(wordListBuilder.buildListToString(itemQty));
     }
 
--- pt1/2.1-simple_modules/kcs.lib/src/module-info.java
+++ pt1/2.1-simple_modules/kcs.lib/src/module-info.java
@@ -1,5 +1,5 @@
 module kcs.lib {
-    requires third.party.lib;
+    requires transitive third.party.lib;
 
     exports kcs.lib;
 }
